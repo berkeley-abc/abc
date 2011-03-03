e881eaf693ff648d78c7b6ca52fc156f0993143e
Alan Mishchenko <alanmi@berkeley.edu>
2011-03-02 18:48:51 -0800

    Removing useless printout in &resim.

--- src/aig/cec/cecSeq.c
+++ src/aig/cec/cecSeq.c
@@ -242,7 +242,7 @@ int Cec_ManSeqResimulateCounter( Gia_Man_t * pAig, Cec_ParSim_t * pPars, Abc_Cex
     Vec_PtrFree( vSimInfo );
     if ( pPars->fVerbose )
         ABC_PRT( "Time", clock() - clkTotal );
-    if ( RetValue )
+    if ( RetValue && pPars->fCheckMiter )
         Abc_Print( 1, "Cec_ManSeqResimulateCounter(): An output of the miter is asserted!\n" );
     return RetValue;
 }
