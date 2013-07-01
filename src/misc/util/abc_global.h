64e2ee223bd6ae2b5950c08e3b1e7c22979509c5
Alan Mishchenko <alanmi@berkeley.edu>
2013-07-01 14:07:42 -0700

    Adding #include <stdlib.h> to 'abc_global.h' to prevent compilation problem on some platforms.

--- src/misc/util/abc_global.h
+++ src/misc/util/abc_global.h
@@ -77,6 +77,7 @@
 
 #include <time.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
 ////////////////////////////////////////////////////////////////////////
 ///                         NAMESPACES                               ///
