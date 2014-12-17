c5162ba6d2ddc5b31a65615902aee0241f62829b
Alan Mishchenko <alanmi@berkeley.edu>
2014-12-16 21:48:16 -0800

    Induced bug with in DFS computation.

--- src/base/abc/abcDfs.c
+++ src/base/abc/abcDfs.c
@@ -89,7 +89,7 @@ Vec_Ptr_t * Abc_NtkDfs( Abc_Ntk_t * pNtk, int fCollectAll )
     vNodes = Vec_PtrAlloc( 100 );
     Abc_NtkForEachObj( pNtk, pObj, i )
     {
-        if ( !Abc_ObjIsCo(pObj) || !Abc_ObjIsBarBuf(pObj) )
+        if ( !Abc_ObjIsCo(pObj) && !Abc_ObjIsBarBuf(pObj) )
             continue;
         Abc_NodeSetTravIdCurrent( pObj );
         Abc_NtkDfs_rec( Abc_ObjFanin0Ntk(Abc_ObjFanin0(pObj)), vNodes );
