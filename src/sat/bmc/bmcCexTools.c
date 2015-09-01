af828a499da1f3357f8b170c9be12f4695190750
Alan Mishchenko <alanmi@berkeley.edu>
2015-09-01 12:02:44 -0700

    Removing unhelpful assertion in CEX minimization.

--- src/sat/bmc/bmcCexTools.c
+++ src/sat/bmc/bmcCexTools.c
@@ -347,7 +347,7 @@ int Bmc_CexVerify( Gia_Man_t * p, Abc_Cex_t * pCex, Abc_Cex_t * pCexCare )
 {
     Gia_Obj_t * pObj;
     int i, k;
-    assert( pCex->nRegs > 0 );
+//    assert( pCex->nRegs > 0 );
 //    assert( pCexCare->nRegs == 0 );
     Gia_ObjTerSimSet0( Gia_ManConst0(p) );
     Gia_ManForEachRi( p, pObj, k )
