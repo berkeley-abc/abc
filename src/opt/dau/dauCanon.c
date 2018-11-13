866f332c5d1264a316da8882446973103e84567d
Anonymous Maarten <anonymous.maarten@gmail.com>
2018-11-13 23:44:22 +0100

    Add pure cmake build system

--- src/opt/dau/dauCanon.c
+++ src/opt/dau/dauCanon.c
@@ -302,7 +302,7 @@ void Abc_TtNormalizeSmallTruth(word * pTruth, int nVars)
     }
 }
 
-inline void Abc_TtVerifySmallTruth(word * pTruth, int nVars)
+void Abc_TtVerifySmallTruth(word * pTruth, int nVars)
 {
 #ifndef NDEBUG
     if (nVars < 6) {
@@ -1419,7 +1419,7 @@ SeeAlso     []
 
 ***********************************************************************/
 
-// Johnson¨CTrotter algorithm
+// Johnson\A8CTrotter algorithm
 static int Abc_NextPermSwapC(char * pData, signed char * pDir, int size)
 {
     int i, j, k = -1;
