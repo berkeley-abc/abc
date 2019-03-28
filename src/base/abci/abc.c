45cdd5acaa4ca44e227fe79c4129555ccd79ff24
Alan Mishchenko <alanmi@berkeley.edu>
2019-03-28 07:50:14 -0700

    Extending simulation interface.

--- src/base/abci/abc.c
+++ src/base/abci/abc.c
@@ -45645,15 +45645,16 @@ usage:
 ***********************************************************************/
 int Abc_CommandAbc9Cfs( Abc_Frame_t * pAbc, int argc, char ** argv )
 {
-    extern void Extra_CommandCfs( Gia_Man_t * pGia, int Limit, int Reps, int UnseenUse, int RareUse, int fReplaceRare, int fVerbose );
+    extern void Extra_CommandCfs( Gia_Man_t * pGia, int Limit, int Reps, int UnseenUse, int RareUse, int fReplaceRare, int fConstSim, int fVerbose );
     int Limit       =   0;
     int Reps        =   1;
     int UnseenUse   =   2;
     int RareUse     =   2;
     int fReplaceRare=   0;
+    int fConstSim   =   0;
     int c, fVerbose =   0;
     Extra_UtilGetoptReset();
-    while ( ( c = Extra_UtilGetopt( argc, argv, "LNURrvh" ) ) != EOF )
+    while ( ( c = Extra_UtilGetopt( argc, argv, "LNURrcvh" ) ) != EOF )
     {
         switch ( c )
         {
@@ -45704,6 +45705,9 @@ int Abc_CommandAbc9Cfs( Abc_Frame_t * pAbc, int argc, char ** argv )
         case 'r':
             fReplaceRare ^= 1;
             break;
+        case 'c':
+            fConstSim ^= 1;
+            break;
         case 'v':
             fVerbose ^= 1;
             break;
@@ -45718,17 +45722,18 @@ int Abc_CommandAbc9Cfs( Abc_Frame_t * pAbc, int argc, char ** argv )
         Abc_Print( -1, "Abc_CommandAbc9Cfs(): There is no AIG.\n" );
         return 1;
     }
-    Extra_CommandCfs( pAbc->pGia, Limit, Reps, UnseenUse, RareUse, fReplaceRare, fVerbose );
+    Extra_CommandCfs( pAbc->pGia, Limit, Reps, UnseenUse, RareUse, fReplaceRare, fConstSim, fVerbose );
     return 0;
 
 usage:
-    Abc_Print( -2, "usage: &cfs [-LNURrvh]\n" );
+    Abc_Print( -2, "usage: &cfs [-LNURrcvh]\n" );
     Abc_Print( -2, "\t          performs simulation\n" );
     Abc_Print( -2, "\t-L num  : the limit on the number of occurrences [default = %d]\n",  Limit );
     Abc_Print( -2, "\t-N num  : the number of repetions of each pattern [default = %d]\n", Reps );
     Abc_Print( -2, "\t-U num  : what to do with unseen patterns [default = %d]\n",         UnseenUse );
     Abc_Print( -2, "\t-R num  : what to do with rare patterns [default = %d]\n",           RareUse );
     Abc_Print( -2, "\t-r      : toggle replacing rare patterns [default = %s]\n",          fReplaceRare? "yes": "no" );
+    Abc_Print( -2, "\t-c      : toggle inserting constants [default = %s]\n",              fConstSim? "yes": "no" );
     Abc_Print( -2, "\t-v      : toggle printing verbose information [default = %s]\n",     fVerbose? "yes": "no" );
     Abc_Print( -2, "\t-h      : print the command usage\n");
     return 1;
--- src/misc/extra/extraUtilCfs.c
+++ src/misc/extra/extraUtilCfs.c
@@ -48,7 +48,7 @@ ABC_NAMESPACE_IMPL_START
   SeeAlso     []
 
 ***********************************************************************/
-void Extra_CommandCfs( Gia_Man_t * pGia, int Limit, int Reps, int UnseenUse, int RareUse, int fReplaceRare, int fVerbose )
+void Extra_CommandCfs( Gia_Man_t * pGia, int Limit, int Reps, int UnseenUse, int RareUse, int fReplaceRare, int fConstSim, int fVerbose )
 {
 }
 
