8e5d0f47026cd760bb2d164ad3f9536f4afa40b6
nianze <nianzelee@gmail.com>
2020-09-21 12:17:04 +0800

    Add a script to delete students' branches

---
+++ lsv/delete-branches.sh
@@ -0,0 +1,7 @@
+students=( $(cut -d, -f1 < lsv/participants-id.csv | tail -n +3) )
+git sw master
+for student in "${students[@]}"; do
+    echo "Deleting branch ${student} ..."
+    git br -d "${student}"
+    git push origin --delete "${student}"
+done
