a817f096e19e3b1dc9938df9d9e421dee06312da
nianze <nianzelee@gmail.com>
2020-09-21 15:41:55 +0800

    Add an external package for LSV commands
    Students are required to develop their solutions in this package.
    An example command to iterate all objects in a network and print their IDs and names was provided.

---
+++ src/ext-lsv/lsvCmd.cpp
@@ -0,0 +1,41 @@
+#include "base/abc/abc.h"
+#include "base/main/main.h"
+#include "base/main/mainInt.h"
+
+static int Lsv_CommandPrintGates( Abc_Frame_t * pAbc, int argc, char ** argv );
+
+void init(Abc_Frame_t* pAbc)
+{
+    Cmd_CommandAdd( pAbc, "LSV", "lsv_print_gates", Lsv_CommandPrintGates, 0);
+}
+
+void destroy(Abc_Frame_t* pAbc)
+{
+}
+
+Abc_FrameInitializer_t frame_initializer = { init, destroy };
+
+struct PackageRegistrationManager
+{
+    PackageRegistrationManager() 
+    {
+        Abc_FrameAddInitializer(&frame_initializer);
+    }
+} lsvPackageRegistrationManager;
+
+int Lsv_CommandPrintGates( Abc_Frame_t * pAbc, int argc, char ** argv )
+{
+    Abc_Ntk_t *pNtk = Abc_FrameReadNtk(pAbc);
+    if (!pNtk) {
+        Abc_Print(-1, "Empty network.\n");
+        return 1;
+    }
+
+    Abc_Obj_t *pObj;
+    int i;
+    Abc_NtkForEachObj(pNtk, pObj, i) {
+        printf("Object Id = %d, name = %s\n", Abc_ObjId(pObj), Abc_ObjName(pObj));
+    }
+
+    return 0;
+}
---
+++ src/ext-lsv/module.make
@@ -0,0 +1,2 @@
+SRC += \
+    src/ext-lsv/lsvCmd.cpp
