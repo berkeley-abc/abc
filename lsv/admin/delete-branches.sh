742bde1d5525466ab54492282fdaca5a07798e77
nianze <nianzelee@gmail.com>
2020-09-28 13:10:44 +0800

    Update scripts to manage branches

--- lsv/admin/delete-branches.sh
+++ lsv/admin/delete-branches.sh
@@ -1,4 +1,4 @@
-students=( $(cut -d, -f1 < lsv/participants-id.csv | tail -n +3) )
+students=( $(cut -d, -f1 < lsv/admin/participants-id.csv | tail -n +3) )
 git sw master
 for student in "${students[@]}"; do
     echo "Deleting branch ${student} ..."
