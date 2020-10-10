afbd5eade1e611a4d0e825b7bd2327447c9a3848
nianze <nianzelee@gmail.com>
2020-10-10 20:51:19 +0800

    Test if lockdown is enabled (disabled) before disabling (enabling) it

--- lsv/admin/disable-lockdown.sh
+++ lsv/admin/disable-lockdown.sh
@@ -1,6 +1,10 @@
-git sw master
-mv .github/workflows/lockdown.yml .github/workflows/lockdown.yml.disabled 
-git a .github/workflows
-git cm "Disable lockdown"
-git push
-lsv/admin/update-branches.sh
+if [ -f ".github/workflows/lockdown.yml" ]; then
+    git sw master
+    mv .github/workflows/lockdown.yml .github/workflows/lockdown.yml.disabled
+    git a .github/workflows
+    git cm "Disable lockdown"
+    git push
+    lsv/admin/update-branches.sh
+else
+    echo "[WARNING] Lockdown is NOT enabled!"
+fi
--- lsv/admin/enable-lockdown.sh
+++ lsv/admin/enable-lockdown.sh
@@ -1,6 +1,10 @@
-git sw master
-mv .github/workflows/lockdown.yml.disabled .github/workflows/lockdown.yml
-git a .github/workflows
-git cm "Enable lockdown"
-git push
-lsv/admin/update-branches.sh
+if [ -f ".github/workflows/lockdown.yml.disabled" ]; then
+    git sw master
+    mv .github/workflows/lockdown.yml.disabled .github/workflows/lockdown.yml
+    git a .github/workflows
+    git cm "Enable lockdown"
+    git push
+    lsv/admin/update-branches.sh
+else
+    echo "[WARNING] Lockdown is NOT disabled!"
+fi
