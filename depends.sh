57b0cbd9370387f3d2fc498dc3e14513abe867c8
William D. Jones <thor0505@comcast.net>
2020-11-19 11:22:55 -0500

    Make depends.sh (s)ccache-aware.

--- depends.sh
+++ depends.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 #echo "## Got: $*"
-CC="$1"
-DIR="$2"
-shift 2
+if [ $1="ccache" -o $1="sccache" ]; then
+    CC="$1 $2"
+    DIR="$3"
+    shift 3
+else
+    CC="$1"
+    DIR="$2"
+    shift 2
+fi
 case "$DIR" in
     "" | ".")
     $CC -MM -MG "$@" | sed -e 's@^\(.*\)\.o:@\1.d \1.o:@'
