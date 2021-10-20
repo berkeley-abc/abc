f24211cc5bd106805531016416a48c6c1e92aa24
hakuchan <kumamoncatch@icloud.com>
2021-10-20 10:33:48 +0900

    first commit

---
+++ cmake-build-debug/CMakeFiles/abc.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/hakuchan/Desktop/original_abc/abc_if_func/src/base/main/main.c" "/Users/hakuchan/Desktop/original_abc/abc_if_func/cmake-build-debug/CMakeFiles/abc.dir/src/base/main/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/hakuchan/Desktop/original_abc/abc_if_func/cmake-build-debug/CMakeFiles/libabc.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/abc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/abc.dir/src/base/main/main.c.o"
+  "abc.pdb"
+  "abc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/abc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
