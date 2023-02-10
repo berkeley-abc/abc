bbd0640db2b0709c06022341690b2b4dc8256612
Alan Mishchenko <alanmi@berkeley.edu>
2023-02-09 16:15:48 -0800

    Enable 'scorr' when AIG has no internal nodes.

--- src/proof/ssw/sswCore.c
+++ src/proof/ssw/sswCore.c
@@ -444,6 +444,7 @@ Aig_Man_t * Ssw_SignalCorrespondence( Aig_Man_t * pAig, Ssw_Pars_t * pPars )
     // if parameters are not given, create them
     if ( pPars == NULL )
         Ssw_ManSetDefaultParams( pPars = &Pars );
+/*
     // consider the case of empty AIG
     if ( Aig_ManNodeNum(pAig) == 0 )
     {
@@ -452,6 +453,7 @@ Aig_Man_t * Ssw_SignalCorrespondence( Aig_Man_t * pAig, Ssw_Pars_t * pPars )
         Aig_ManReprStart( pAig,Aig_ManObjNumMax(pAig) );
         return Aig_ManDupOrdered(pAig);
     }
+*/
     // check and update parameters
     if ( pPars->fLatchCorrOpt )
     {
