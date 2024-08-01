9f864ebe76047d9e4fca8a6c209e5c92d3da448a
Alan Mishchenko <alanmi@berkeley.edu>
2024-07-31 22:25:01 -0700

    Trying to fix the compilation issue.

--- src/aig/gia/giaUtil.c
+++ src/aig/gia/giaUtil.c
@@ -48,8 +48,13 @@ ABC_NAMESPACE_IMPL_START
 ***********************************************************************/
 unsigned Gia_ManRandom( int fReset )
 {
+#ifdef _MSC_VER
+    static unsigned int m_z = NUMBER1;
+    static unsigned int m_w = NUMBER2;
+#else
     static __thread unsigned int m_z = NUMBER1;
     static __thread unsigned int m_w = NUMBER2;
+#endif    
     if ( fReset )
     {
         m_z = NUMBER1;
